bd98e32c275cab99c984971a7dfdb877b69b4c06
Pomarancz789 <granat-02krawcy@icloud.com>
2022-05-25 09:36:40 +0200

    Initial Commit

---
+++ obliczenie_pi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  obliczenie_pi
+//
+//  Created by Jakub Wtorkiewicz on 25/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
