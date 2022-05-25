002357c761a09d8af032859d0e312edc0237f6fc
Pomarancz789 <granat-02krawcy@icloud.com>
2022-05-25 09:56:04 +0200

    popełniam zadanie z lekcji

--- obliczenie_pi/main.cpp
+++ obliczenie_pi/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  obliczenie_pi
-//
-//  Created by Jakub Wtorkiewicz on 25/05/2022.
-//
-
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main(){
+    srand( time( NULL ) );
+    float accuracy=1000000.0;
+    float wewnetrzne=0.0;
+    
+    for (int i=0; i<accuracy; i++) {
+        float x=0+((float)rand()/(RAND_MAX))*(1-0);
+        float y=0+((float)rand()/(RAND_MAX))*(1-0);
+        if ((x*x+y*y)<=1) {
+            wewnetrzne++;
+        }
+    }
+    
+    float pi = 4*(wewnetrzne/accuracy);
+    
+    cout << pi <<endl;
+    
     return 0;
 }
